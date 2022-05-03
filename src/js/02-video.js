0d22624a766b9ac384e404afc983735d06d40408
Alexandra Sabatin <s4batin@gmail.com>
2022-05-03 04:44:46 +0300

    add saving the current video time
    install Vimeo Player API and lodash.throttle libraries

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onPlay, 1000));
+addEventListener('DOMContentLoaded', pageInit);
+
+function onPlay(data) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds));
+    // console.log(JSON.stringify(data.seconds))
+};
+
+function pageInit() {
+    const savedTime = localStorage.getItem(STORAGE_KEY);
+
+    if (savedTime) {
+        player.setCurrentTime(savedTime);
+    } 
+}
